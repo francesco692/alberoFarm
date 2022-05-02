b29a9afaf0dea5b599cf150b4b91a02faacd5c37
Francesco <francesco.serra8280@outlook.it>
2022-05-02 13:00:25 +0200

    added my project

---
+++ src/main/java/com/exalfa/alberoFarm/AlberoFarmApplication.java
@@ -0,0 +1,13 @@
+package com.exalfa.alberoFarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlberoFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlberoFarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exalfa/alberoFarm/AlberoFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exalfa.alberoFarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlberoFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
